08263a04d28a1382df7534d61a8e5a5be649cb60
anis boulila <boulilaa247@gmail.com>
2026-06-04 21:32:32 +0100

    make the page in container
    replace tailwidcss container with my custom container

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,9 +1,9 @@
 import './styles.css'
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar/Navbar'
 
 export default async function HomePage() {
   return (
-    <div className="container mx-auto">
+    <div className="lg:max-w-[70%] md:max-w-[75%] sm:max-w-[80%] max-w-[90%] mx-auto">
       <Navbar />
     </div>
   )
