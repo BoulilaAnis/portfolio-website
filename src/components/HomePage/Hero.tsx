fbdb24f9a7df5606636c0dc03e14363e23b2a816
anis boulila <boulilaa247@gmail.com>
2026-06-11 20:51:33 +0100

    fix TextEffect error

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -28,54 +28,52 @@ const Hero = async () => {
           </div>
         </div>
         <div className="w-[79%] mx-auto">
-          
           <h1
             className={`${quicksand.className} md:text-xl text-md text-secondary-foreground bg-secondary rounded-xl py-3 px-5 text-center mt-9 sm:w-full w-[90%] mx-auto`}
           >
-            <TextEffect per='char' preset='fade'>
-              {hero.welcomeBanner}
+            <TextEffect per="char" preset="fade">
+              {hero.welcomeBanner || ''}
             </TextEffect>
           </h1>
           <div className="flex sm:flex-row sm:gap-2 gap-9 sm:justify-between justify-center items-center mt-9 flex-col">
             {' '}
             <div className="w-fit">
               <h3 className={`${quicksand.className} text-5xl font-bold`}>Anis Boulila</h3>
-              <span className='tracking-wide'>
+              <span className="tracking-wide">
                 Craftsman ({' '}
                 <TextLoop
-                  className='overflow-y-clip text-primary tracking-wide'
+                  className="overflow-y-clip text-primary tracking-wide"
                   transition={{
-                           type: 'spring',
-                           stiffness: 900,
-                           damping: 80,
-                           mass: 10,
-                         }}
-                         variants={{
-                           initial: {
-                             y: 20,
-                             rotateX: 90,
-                             opacity: 0,
-                             filter: 'blur(4px)',
-                           },
-                           animate: {
-                             y: 0,
-                             rotateX: 0,
-                             opacity: 1,
-                             filter: 'blur(0px)',
-                           },
-                           exit: {
-                             y: -20,
-                             rotateX: -90,
-                             opacity: 0,
-                             filter: 'blur(4px)',
-                           },
-                         }}
+                    type: 'spring',
+                    stiffness: 900,
+                    damping: 80,
+                    mass: 10,
+                  }}
+                  variants={{
+                    initial: {
+                      y: 20,
+                      rotateX: 90,
+                      opacity: 0,
+                      filter: 'blur(4px)',
+                    },
+                    animate: {
+                      y: 0,
+                      rotateX: 0,
+                      opacity: 1,
+                      filter: 'blur(0px)',
+                    },
+                    exit: {
+                      y: -20,
+                      rotateX: -90,
+                      opacity: 0,
+                      filter: 'blur(4px)',
+                    },
+                  }}
                 >
                   {hero.CraftsmanBio.map((list) => (
                     <span>{list.skill}</span>
                   ))}
-                </TextLoop>
-                {' '}
+                </TextLoop>{' '}
                 )
               </span>
               <WavyLine />
